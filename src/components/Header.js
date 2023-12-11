9681875997a6928774066ffd1ee5fd0b06f601c4
jcrunatay <juyanrunatay@gmail.com>
2023-12-11 12:19:07 -0500

    fixed logo animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ export default function Header(){
             </video>
             <header>
                 <nav className={ scrolled ? "scrolled" : "" }>
-                <a className="nav-logo-container" href="./index.html"><img className="nav-logo" src="./images/logo.png" alt="logo" /></a> 
+                    <a className="nav-logo-container" href="./index.html"><img className="nav-logo" src="./images/logo.png" alt="logo" /></a> 
                     <ul>
                         <li><a href="#home">Home</a></li>
                         <li><a href="#about">About</a></li>
