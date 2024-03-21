f87f9c3192de4d9d18d10a3681413062dbc76598
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-03-21 13:55:33 -0400

    added filter drop shadow on image

--- src/components/About.js
+++ src/components/About.js
@@ -38,7 +38,7 @@ export default function About() {
                 ref={divRef}
             >
                 <div className="about-leftDiv">
-                    <img className="profile" src="./images/profile.png" alt="profile" />
+                    <img className="profile" src="./images/profile-no-bg.png" alt="profile" />
                 </div>
                 <div className="about-rightDiv">
                     <div className="bio-container">
