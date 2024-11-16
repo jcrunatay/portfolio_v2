02f6d749949cc6ab3638196093b472b751723264
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-11-15 20:05:06 -0500

    Updated Resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,7 +9,7 @@ export default function Resume() {
                 <a
                     className="resume-btn"
                     rel="noreferrer"
-                    href="./images/document/Juyan_Criston_Runatay_CV.pdf"
+                    href="./images/document/Juyan_Criston_Runatay_Resume.pdf"
                     target="_blank"
                 >
                     My Resume
